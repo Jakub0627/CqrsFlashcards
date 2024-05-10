f0de2989cd138cca6f464bd35d3995c3bf710587
Jakub <kuba.gladysz@hotmail.com>
2024-05-10 21:30:56 +0200

    Setting up cqrsFlashcard app template

---
+++ CqrsFlashcards/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CqrsFlashcards.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CqrsFlashcards.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Flashcard> Flashcards { get; set; }
+    }
+}
---
+++ CqrsFlashcards/Models/Flashcard.cs
@@ -0,0 +1,9 @@
+﻿namespace CqrsFlashcards.Models
+{
+    public class Flashcard
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
